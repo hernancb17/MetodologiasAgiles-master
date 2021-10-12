3d3a44a8d65dcd2d0a88521aacde9b9b1887693a
hernancb17 <hbasso@primerosystems.com>
2021-10-12 19:54:49 -0300

    Refactor

---
+++ TPAgilesGrupo1/Model/Ahorcado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPAgilesGrupo1.Model
+{
+    public class Ahorcado
+    {
+        private const string PalabraCorrecta = "METODOLOGIAS AGILES";
+
+        public bool ValidarIntento(string intento)
+        {
+            return intento.ToUpper().Equals(PalabraCorrecta);
+        }
+    }
+}
---
+++ TPAgilesGrupo1/Model/Invitado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TPAgilesGrupo1.Model
+{
+    public class Invitado
+    {
+        public bool ValidarIngreso(string nombreIngresado)
+        {
+            if (nombreIngresado.Length > 2 && nombreIngresado.Length < 20)
+            {
+                Regex expresionRegular = new Regex(@"[a-zA-Z0-9]");
+
+                var devolucion = expresionRegular.Matches(nombreIngresado);
+
+                return devolucion.Count == nombreIngresado.Length ? true : false;
+            }
+
+            return false;
+        }
+    }
+}
